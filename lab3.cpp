5d6f9def288a2502f5b70b37f514f044f9b9ef10
maulik <maulikchhabra@gmail.com>
2019-01-17 11:12:51 +0530

    class declaration

---
+++ lab3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+class Calc
+{       public:
+	    int a,b;
+        int substract(int a ,int b);
+        int multiply(int a ,int b);
+        int add(int a ,int b);
+		float divide(int a ,int b);
+};
+int main()
+{
+
+}
+
+
+
+
