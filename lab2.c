7953e2b267ac899a91a8a5f13c7482b4dc8ea373
maulik <maulikchhabra@gmail.com>
2019-02-03 19:27:44 +0530

    simple c program

--- lab2.c
+++ lab2.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int n;
+	int n;//initilalised a variable//
 	printf("hello world");
 	printf("Enter any number");
-	scanf("%d",&n);
+	scanf("%d",&n);//storing the variable//
 	printf("Your number is %d",n);
 	return 0;
 
