b28d1d1723642fa45e31df51525744c8340b7970
maulik <maulikchhabra@gmail.com>
2019-02-12 23:11:23 +0530

    Took numbers as input to perform operations

--- lab3.cpp
+++ lab3.cpp
@@ -5,13 +5,28 @@ class Calc
         int substract(int a ,int b);
         int multiply(int a ,int b);
         int add(int a ,int b);
-		float divide(int a ,int b);
+        float divide(int a ,int b);
 };
 int main()
 {
-
+   int func;
+   int firstnumber,secondnumber,result;
+   
+   cout<<"1.substract"<<endl; 
+   cout<<"2.multiply"<<endl;
+   cout<<"3.add"<<endl;
+   cout<<"4.divide"<<endl;
+   cout<<"Which operator you want to use to perform calculations="<<endl;
+   cin>>func;
+   cout<<endl;
+   cout<<"Enter first number="<<endl;
+   cin>>firstnumber;
+   cout<<"Enter second number="<<endl;
+   cin>>secondnumber;
+   cout<<"1st number="<<firstnumber<<endl;
+   cout<<"2nd number="<<secondnumber<<endl;
 }
-
+   
 
 
 
