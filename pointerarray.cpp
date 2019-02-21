bcb596abc3886bb9ef2fe1989658e731448d6a47
maulik <maulikchhabra@gmail.com>
2019-02-21 10:31:17 +0530

    Comments added to  the program

--- pointerarray.cpp
+++ pointerarray.cpp
@@ -6,15 +6,15 @@ class pointerarray
 };
 int main()
 {
-	int arr[5],i;
-	int sum,sub,mul,div,increment,decrement;
+	int arr[5],i; //array and array element declaration//
+	int sum,sub,mul,div,increment,decrement; //operation declaration//
 	cout<<"Enter the elements=";
-	for(i=0;i<5;i++)
+	for(i=0;i<5;i++) //for loop to input array//
 	{
 		cin>>arr[i];
 	}
 	cout<<"Elements are="<<endl;
-	for(i=0;i<5;i++)
+	for(i=0;i<5;i++) //for loop to show the array//
 	{
 		cout<<arr[i]<<endl;
 	}
@@ -23,15 +23,15 @@ int main()
 	cout<<"sum is ="<<sum<<endl;
 	cout<<"SUBSTRACTION IS DONE ON ="<<*(arr+4)<<*(arr+1)<<endl;
 	sub=*(arr+4)-*(arr+1);
-    cout<<"sub is ="<<sub<<endl;
-    cout<<"MULTIPLICATION IS DONE ON ="<<*(arr+1)<<*(arr+2)<<endl;
-    mul=*(arr+1)*(*(arr+2));
-    cout<<"mul is ="<<mul<<endl;
-    cout<<"INCREMENT IS DONE ON ="<<*(arr+1)<<endl;
-    increment=++(*(arr+1));
-    cout<<"increment is ="<<increment<<endl;
-    cout<<"DECREMENT IS DONE ON ="<<increment<<endl;
-    decrement=--(*(arr+1));
-    cout<<"decrement is ="<<decrement<<endl;
+        cout<<"sub is ="<<sub<<endl;
+        cout<<"MULTIPLICATION IS DONE ON ="<<*(arr+1)<<*(arr+2)<<endl;
+        mul=*(arr+1)*(*(arr+2));
+        cout<<"mul is ="<<mul<<endl;
+        cout<<"INCREMENT IS DONE ON ="<<*(arr+1)<<endl;
+        increment=++(*(arr+1));
+        cout<<"increment is ="<<increment<<endl;
+        cout<<"DECREMENT IS DONE ON ="<<increment<<endl;
+        decrement=--(*(arr+1));
+        cout<<"decrement is ="<<decrement<<endl;
  return 0;   
 }
